b4af0673a4c4c7bb1cf43a628977343ecbb58308
Leonardo Javier Alviarez Hernández <alviarez.leonardo@gmail.com>
2019-04-25 05:03:38 -0400

    Added movie detail functionality

--- src/app/pages/movie-details/movie-details.page.ts
+++ src/app/pages/movie-details/movie-details.page.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+
+import { MovieService } from './../../services/movie.service';
 
 @Component({
   selector: 'app-movie-details',
@@ -7,9 +10,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MovieDetailsPage implements OnInit {
 
-  constructor() { }
+  information = null;
+
+  /**
+   * Constructor of our details page
+   * @param ActivatedRoute Information about the route we are on
+   * @param MovieService The movie Service to get data
+   */
+  constructor(private activatedRoute: ActivatedRoute, private movieService: MovieService) { }
 
   ngOnInit() {
+    // Get the ID that was passed with the URL
+    let id = this.activatedRoute.snapshot.paramMap.get('id');
+
+    // Get the information from the API
+    this.movieService.getDetails(id).subscribe(result => {
+      this.information = result;
+    });
   }
 
+  openWebsite() {
+    window.open(this.information.Website, '_blank');
+  }
 }
